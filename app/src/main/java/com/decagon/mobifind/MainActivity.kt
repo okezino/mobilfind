c166731689dcc0ebfaa1921bd401255cf787a68c
Victor Adewumi <samuel.ochuba@decagon.dev>
2021-05-19 20:00:09 +0100

    first commit

---
+++ app/src/main/java/com/decagon/mobifind/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.decagon.mobifind
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
